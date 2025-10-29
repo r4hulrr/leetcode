ab974b5dc6fe74c1edabb937b310e0dac27fca8b
Rahul Ramachandran <rahulram07r@gmail.com>
2025-10-29 11:15:22 +1100

    added sols for 26, 27 and 83

---
+++ 27/RemoveElement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if(nums.empty()) return 0;
+        int slow = 0;
+        int fast = 0;
+        while(fast < nums.size()){
+            if (nums[fast] != val){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+};
